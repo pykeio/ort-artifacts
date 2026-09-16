f0286fa163399e100bcf7efee2f9322407bce6e7
Onuralp SEZER <thunderbirdtr@gmail.com>
2026-09-16 16:54:19 +0300

    Add sm_120 to CUDA architectures for Blackwell GPUs
    Signed-off-by: Onuralp SEZER <thunderbirdtr@gmail.com>

--- src/build.ts
+++ src/build.ts
@@ -217,7 +217,7 @@ await new Command()
 			args.push('-Donnxruntime_USE_FP8_KV_CACHE=OFF');
 			args.push('-Donnxruntime_QUICK_BUILD=ON');
 
-			args.push('-DCMAKE_CUDA_ARCHITECTURES=75;80;90');
+			args.push('-DCMAKE_CUDA_ARCHITECTURES=75;80;90;120');
 			cudaFlags.push('-compress-mode=size');
 		}
 
